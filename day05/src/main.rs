6c286190e5066d9394efba95682e57d646406949
D071283 <lena.hoinkis@sap.com>
2022-12-06 15:24:02 +0100

    refactor

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,53 +1,64 @@
 use std::fs;
 
-
 fn main() {
-   //Read Stacks
-   let input = fs::read_to_string("input.txt").unwrap();
-   //(crate,order,char)
-   let emptyVec = Vec::new();
-   let mut stacks: Vec<Vec<_>> = vec![emptyVec; 9];  
+    //Read Stacks
+    let input = fs::read_to_string("input.txt").unwrap();
+    //(crate,order,char)
+    let emptyVec = Vec::new();
+    let mut stacks: Vec<Vec<_>> = vec![emptyVec; 9];
     let mut start_of_commands = 0;
 
-   //fill stack
-   for (i,line) in input.lines().enumerate() {
-        let x:Vec<(usize,char)> = line.char_indices()
+    //fill stack
+    for (i, line) in input.lines().enumerate() {
+        let x: Vec<(usize, char)> = line
+            .char_indices()
             .filter(|c| c.1.is_ascii_alphabetic())
-            .map(|c| ((c.0 + 2 )/4 , c.1))
+            .map(|c| ((c.0 + 2) / 4, c.1))
             .collect();
-            
-        if x.is_empty(){
-            start_of_commands = i+1;
-            break
-        }       
+
+        if x.is_empty() {
+            start_of_commands = i + 1;
+            break;
+        }
         for c in x {
             stacks[c.0].push(c.1)
         }
     }
     stacks.iter_mut().for_each(|x| x.reverse());
+    let mut stacks_copy = stacks.clone();
 
-    for (i,line) in input.lines().enumerate() {
-        if i>=start_of_commands+1{
+    for (i, line) in input.lines().enumerate() {
+        if i >= start_of_commands + 1 {
             // amount, old, new
-            let command:Vec<usize> = line.split_ascii_whitespace()
-            .map(|c| c.to_string().parse::<usize>())
-            .filter(|c|   !c.is_err())
-            .map(|c| c.unwrap())
-            .collect();
-/*
+            let command: Vec<usize> = line
+                .split_ascii_whitespace()
+                .map(|c| c.to_string().parse::<usize>())
+                .filter(|c| !c.is_err())
+                .map(|c| c.unwrap())
+                .collect();
+
             //part1
             for _ in 0..command[0] {
-                let popped = stacks[command[1]-1].pop();
+                let popped = stacks_copy[command[1] - 1].pop();
                 match popped {
-                    Some(i) => stacks[command[2]-1].push(i),
-                    _ => unreachable!()
+                    Some(i) => stacks_copy[command[2] - 1].push(i),
+                    _ => unreachable!(),
                 }
-            }    */
-            //part2 
-            let tail = stacks[command[1]-1].len() - command[0];
-            let mut drained:Vec<_>  = stacks[command[1]-1].drain(tail..).collect();
-            stacks[command[2]-1].append(&mut drained);
-            }   
+            }
+            //part2
+            let tail = stacks[command[1] - 1].len() - command[0];
+            let mut drained: Vec<_> = stacks[command[1] - 1].drain(tail..).collect();
+            stacks[command[2] - 1].append(&mut drained);
+        }
     }
-    println!("({:?})",stacks);
+    println!("{}", get_code(stacks));
+    println!("{}", get_code(stacks_copy));
+}
+
+fn get_code(vec: Vec<Vec<char>>) -> String {
+    let mut buf = Vec::new();
+    for ele in vec {
+        buf.push(ele.last().unwrap().clone());
+    }
+    buf.into_iter().collect()
 }
--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,32 +1,34 @@
-
-use std::fs;
 use std::collections::HashSet;
+use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    println!("{}",part1(&input));
-    println!("{}",part2(&input));
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
 }
 
 fn part1(s: &str) -> usize {
-    calc_marker(3,s)
+    calc_marker(3, s)
 }
 
 fn part2(s: &str) -> usize {
-    calc_marker(13,s)
+    calc_marker(13, s)
 }
 
-fn calc_marker(steps: usize, s: &str)-> usize{
-    let result = s.char_indices()
-    .skip(steps)
-    .take_while(|x| !is_marker(&s[x.0-steps..x.0+1])).last();
+fn calc_marker(steps: usize, s: &str) -> usize {
+    let result = s
+        .char_indices()
+        .skip(steps)
+        .take_while(|x| !is_marker(&s[x.0 - steps..x.0 + 1]))
+        .last();
+
     match result {
-        Some(i) => return i.0+2,
-        _ => unreachable!()
+        Some(i) => return i.0 + 2,
+        _ => unreachable!(),
     }
 }
 
-fn is_marker(s : &str) -> bool{
+fn is_marker(s: &str) -> bool {
     let mut uniq = HashSet::new();
     s.chars().into_iter().all(move |x| uniq.insert(x))
 }
@@ -36,11 +38,11 @@ mod tests {
     use super::*;
     #[test]
     fn test_part1() {
-            assert_eq!(part1("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
-            assert_eq!(part1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
-            assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
-            assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
-        }
+        assert_eq!(part1("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(part1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
     #[test]
     fn test_part2() {
         assert_eq!(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
@@ -53,8 +55,8 @@ mod tests {
     fn test_is_marker() {
         assert_eq!(is_marker("aaaa"), false);
         assert_eq!(is_marker("abxd"), true);
-   
+
         let s = "abca";
         assert_eq!(is_marker(&s[0..3]), true);
     }
-    }
+}
