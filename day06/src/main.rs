cdcc3578621ba77ded442d0196542c6330c93f96
D071283 <lena.hoinkis@sap.com>
2022-12-06 11:59:31 +0100

    add part2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -5,12 +5,21 @@ use std::collections::HashSet;
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     println!("{}",part1(&input));
+    println!("{}",part2(&input));
 }
 
 fn part1(s: &str) -> usize {
+    calc_marker(3,s)
+}
+
+fn part2(s: &str) -> usize {
+    calc_marker(13,s)
+}
+
+fn calc_marker(steps: usize, s: &str)-> usize{
     let result = s.char_indices()
-        .skip(3)
-        .take_while(|x| !is_marker(&s[x.0-3..x.0+1])).last();
+    .skip(steps)
+    .take_while(|x| !is_marker(&s[x.0-steps..x.0+1])).last();
     match result {
         Some(i) => return i.0+2,
         _ => unreachable!()
@@ -32,6 +41,14 @@ mod tests {
             assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
             assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
         }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+        assert_eq!(part2("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+        assert_eq!(part2("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+        assert_eq!(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+        assert_eq!(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
+    }
     #[test]
     fn test_is_marker() {
         assert_eq!(is_marker("aaaa"), false);
