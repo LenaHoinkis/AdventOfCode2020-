49371861dd89553b4eea1f45b63832a515a08cf4
D071283 <lena.hoinkis@sap.com>
2022-12-06 11:34:15 +0100

    day06 part1

---
+++ day06/src/main.rs
@@ -0,0 +1,43 @@
+
+use std::fs;
+use std::collections::HashSet;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    println!("{}",part1(&input));
+}
+
+fn part1(s: &str) -> usize {
+    let result = s.char_indices()
+        .skip(3)
+        .take_while(|x| !is_marker(&s[x.0-3..x.0+1])).last();
+    match result {
+        Some(i) => return i.0+2,
+        _ => unreachable!()
+    }
+}
+
+fn is_marker(s : &str) -> bool{
+    let mut uniq = HashSet::new();
+    s.chars().into_iter().all(move |x| uniq.insert(x))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_part1() {
+            assert_eq!(part1("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+            assert_eq!(part1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+            assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+            assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+        }
+    #[test]
+    fn test_is_marker() {
+        assert_eq!(is_marker("aaaa"), false);
+        assert_eq!(is_marker("abxd"), true);
+   
+        let s = "abca";
+        assert_eq!(is_marker(&s[0..3]), true);
+    }
+    }
